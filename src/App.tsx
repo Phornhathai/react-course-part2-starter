4abf769c12bac82da60376ce0d6c0e2fccb22a99
Phornhathai <phornhathai.aum@gmail.com>
2026-09-14 11:55:50 +0700

    add QueryClient, QueryClientProvider, ReactQueryDevtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import "./App.css";
+import PostList from "./react-query/PostList";
+import PostListPagination from "./react-query/PostListPagination";
+import PostListLoadmore from "./react-query/PostListLoadmore";
 import TodoList from "./react-query/TodoList";
+import TodoForm from "./react-query/TodoForm";
 
 function App() {
-  return <TodoList />;
+  return (
+    <>
+      <TodoForm />
+      <TodoList />
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,31 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
 
 // create new instance
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       retry: 3,
+//       gcTime: 300_000, //5m
+//       staleTime: 10 * 1000, // 10s
+//       refetchOnWindowFocus: false,
+//       refetchOnReconnect: false,
+//       refetchOnMount: false
+//     }
+//   }
+// });
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>,
 );
